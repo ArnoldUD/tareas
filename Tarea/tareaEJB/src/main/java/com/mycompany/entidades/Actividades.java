11bc0cd72573f1d926fe5e4f8bd49a126c95e4e3
ArnoldUD <ArnoldUD@LESTAD>
2019-04-26 20:55:26 -0500

    Cambio el nombre de autor de las clases de entidades correspondiente al usuario de github

--- Tarea/tareaEJB/src/main/java/com/mycompany/entidades/Actividades.java
+++ Tarea/tareaEJB/src/main/java/com/mycompany/entidades/Actividades.java
@@ -23,7 +23,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  *
- * @author usuario
+ * @author ArnoldUD
  */
 @Entity
 @Table(name = "ACTIVIDADES")
--- Tarea/tareaEJB/src/main/java/com/mycompany/entidades/Responsables.java
+++ Tarea/tareaEJB/src/main/java/com/mycompany/entidades/Responsables.java
@@ -22,7 +22,7 @@ import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
- * @author usuario
+ * @author ArnoldUD
  */
 @Entity
 @Table(name = "RESPONSABLES")
